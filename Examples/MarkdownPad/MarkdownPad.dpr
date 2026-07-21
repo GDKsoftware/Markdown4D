8dfad896f35bb55f1294256836610e64b93c020f
cmgeuze <cmgeuze@gdksoftware.nl>
2026-07-21 19:47:42 +0200

    feat: full-featured MarkdownPad demos on VCL and FMX (tabs, session restore, recent files, drag-and-drop, file watching, view modes, command palette, zen mode, find bar, HTML export, flat icon toolbar)

--- Examples/MarkdownPad/MarkdownPad.dpr
+++ Examples/MarkdownPad/MarkdownPad.dpr
@@ -2,6 +2,12 @@ program MarkdownPad;
 
 uses
   Vcl.Forms,
+  MarkdownPad.Workspace.Interfaces in '..\Shared\MarkdownPad.Workspace.Interfaces.pas',
+  MarkdownPad.Workspace in '..\Shared\MarkdownPad.Workspace.pas',
+  MarkdownPad.Session in '..\Shared\MarkdownPad.Session.pas',
+  MarkdownPad.Commands in '..\Shared\MarkdownPad.Commands.pas',
+  MarkdownPad.FileWatcher in '..\Shared\MarkdownPad.FileWatcher.pas',
+  MarkdownPad.HtmlExport in '..\Shared\MarkdownPad.HtmlExport.pas',
   MarkdownPad.Main in 'MarkdownPad.Main.pas';
 
 {$R *.res}
